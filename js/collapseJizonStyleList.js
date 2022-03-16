13ed5f77df7f32b16fc94eb7cb98bcdcb361eb11
flashgift <flashgift80@gmail.com>
2022-03-16 12:15:38 +0900

    다섯번째 커미잇

--- js/collapseJizonStyleList.js
+++ js/collapseJizonStyleList.js
@@ -2,16 +2,40 @@ var elements = document.getElementsByClassName("tree-child-icon");
 for(var item of elements)
 {
     let nitem = item.parentElement.parentElement.children[1];
+    nitem.addEventListener("transitionend", (e) => {
+        treeSpreadDone(e, nitem);
+    });
     item.onclick = (e) => {
-        treeisclick(e, nitem);
+       toggleHeight(e, nitem);
     };
 }
 
+function getElementOriginalHeight(element){
+    let resultheight = 0;
+    for(let item of element.children)
+    {
+        resultheight += item.clientHeight;
+    }
+    return resultheight;
+}
 
-
-function treeisclick(e, elementList) {
-    if(elementList.style.display !== "none")
-        elementList.style.display = "none";
+function toggleHeight(e, elementList) {
+    if(elementList.style.height !== "0px")
+    {
+        elementList.style.height = getElementOriginalHeight(elementList)+"px"; 
+        setTimeout(() => {elementList.style.height = "0px";}, 5);
+    }
     else
-        elementList.style.display = "contents";
+    {
+        elementList.style.height = "0px"; 
+        elementList.style.display = "flex";
+        setTimeout(() => {elementList.style.height = getElementOriginalHeight(elementList)+"px";}, 5);
+    }     
+}
+
+function treeSpreadDone(e, elementList) {
+    if(elementList.style.height !== "0px")
+        elementList.style.height = "auto";
+    else
+        elementList.style.display = "none";
 }
